54e416688d1f3b3c832141dfe6a02e660ab40bf1
burakgunce <brk.gnc9@hotmail.com>
2025-01-08 23:47:30 +0300

    transaction pipeline

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Transaction;
 using Domain.Entities;
 using MediatR;
 using System;
@@ -11,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Commands.Create;
 
-public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>, ITransactionalRequest
 {
     public string Name { get; set; }
 
@@ -34,8 +35,12 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
             
             Brand brand = _mapper.Map<Brand>(request);
             brand.Id = Guid.NewGuid();
-
+            
+            //Brand brand2 = _mapper.Map<Brand>(request);
+            //brand2.Id = Guid.NewGuid();
+                                                           //comments for trying transaction scope
             await _brandRepository.AddAsync(brand);
+            //await _brandRepository.AddAsync(brand2);
 
             CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(brand);
             
