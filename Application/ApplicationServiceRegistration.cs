7cf670537ceed6e4e0a7ebc24500b3b5d62558c6
burakgunce <brk.gnc9@hotmail.com>
2025-01-17 00:15:51 +0300

    mssql logging

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -42,7 +42,8 @@ public static class ApplicationServiceRegistration
             configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
-        services.AddSingleton<LoggerServiceBase, FileLogger>();
+        //services.AddSingleton<LoggerServiceBase, FileLogger>();
+        services.AddSingleton<LoggerServiceBase, MsSqlLogger>();
 
         return services;
     }
